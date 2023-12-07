287ac7f1a3b1b8e052ba4303333fcb4b67300e41
Andrew Ceniccola <andrewceniccola@Andrews-MBP.home>
2023-12-07 17:48:43 -0500

    projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,48 +1,46 @@
 import * as React from 'react';
 import Button from '@mui/joy/Button';
 import Card from '@mui/joy/Card';
-import { Link }  from 'react-router-dom';
+import Link  from '@mui/joy/Link';
 import CardContent from '@mui/joy/CardContent';
 import IconButton from '@mui/joy/IconButton';
 import Typography from '@mui/joy/Typography';
 import BookmarkAdd from '@mui/icons-material/BookmarkAddOutlined';
 import  GitHubIcon  from '@mui/icons-material/GitHub';
 import CardMedia from '@mui/material/CardMedia';
+import cajal from '../static/cajal.jpg';
 import { CardActionArea, CardActions } from '@mui/material';
 
 
 function Projects(){
-    return (
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="100"
-              image="../static/cajal.jpg"
-              alt="cajal"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Cajal
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Neural Network Library for quick machine learning in c++.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <IconButton 
-              component={Link} 
-              to="https://github.com/andarklabs/cajal"
-              sx = {{
-                  p:'8px',
-                  backgroundColor:'#CCCCCC',
-                  marignLeft: 'auto',
-              }}><GitHubIcon color="secondary"/>
 
-              </IconButton>
-          </CardActions>
-        </Card>
-      );
+  return (
+    <Card variant = "soft" sx={{ maxWidth: 225, maxHeight:250}} style = {{ backgroundColor: 'rgba(0,0,0, 0.5)' }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140" // Adjust the height as needed
+          image={cajal} // Use the imgUrl variable instead of the img element
+          alt="cajal"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            <Link
+            overlay
+            underline = "none"
+            href = ""
+            >
+            Cajal
+            </Link>
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Neural Network Library for quick machine learning in C++.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+
 }
+
 export default Projects;
