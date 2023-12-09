db1cc5ecdc0771356338f3d5b03163201b07a7dc
Andrew Ceniccola <andrewceniccola@Andrews-MacBook-Pro.local>
2023-12-09 13:43:51 -0500

    portfolio

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,6 +2,9 @@ import * as React from 'react';
 import BookmarkAdd from '@mui/icons-material/BookmarkAddOutlined';
 import  GitHubIcon  from '@mui/icons-material/GitHub';
 import cajal from '../static/cajal.jpg';
+import hyper from '../static/hyperjumps.png';
+import website from '../static/website.png';
+import robdoc from '../static/robot_doctor.jpeg';
 import { Card, Link, Typography, CardContent, Button, IconButton} from '@mui/joy';
 import { CardActionArea, Stack, Paper, CardMedia } from '@mui/material';
 
@@ -28,55 +31,53 @@ function Projects(){
       </Paper>
       <Stack spacing={5} alignItems="center" style={{ marginTop: '55px',}}>
         <Stack direction='row'  spacing={5} alignItems="center" >
-          <Card variant = "outlined" sx={{ maxWidth: 200, maxHeight:220}} style = {{ backgroundColor: '#90a4ae', boxShadow: '10px 10px 10px rgba(0,0,0, 0.99)', }} color = "#FFF">
+          <Card variant = "soft" color= 'primary' sx={{ maxWidth: 200, minWidth: 200, maxHeight:220, minHeight:220}} style = {{ backgroundColor: '#CCCCCC', boxShadow: '10px 10px 10px rgba(0,0,0, 0.99)', }}>
             <CardActionArea>
               <CardMedia
                 component="img"
                 height="100" // Adjust the height as needed
                 image={cajal} // Use the imgUrl variable instead of the img element
-                alt="cajal"
+                alt="artistic interpretation of biological neural network"
               />
               <CardContent>
-                <Typography gutterBottom variant="h5" component="div" color = "#000">
+                <Typography gutterBottom variant="h5" component="div" >
                   <Link
                   overlay
                   underline = "none"
                   rel="noreferrer" // this is bad for anaylitics but good security
                   href = "https://github.com/andarklabs/cajal"
-                  color='#000'
                   target="_blank"
                   >
                   Cajal
                   </Link>
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant="body2" color="neutral">
                   Neural Network Library for quick machine learning in C++.
                 </Typography>
               </CardContent>
             </CardActionArea>
           </Card>
-          <Card variant = "outlined" sx={{ maxWidth: 200, maxHeight:220}} style = {{ backgroundColor: '#90a4ae', boxShadow: '10px 10px 10px rgba(0,0,0, 0.99)', }} color = "#FFF">
+          <Card variant = "soft" color= 'primary' sx={{ maxWidth: 200, minWidth: 200, maxHeight:220, minHeight:220}} style = {{ backgroundColor: '#CCCCCC', boxShadow: '10px 10px 10px rgba(0,0,0, 0.99)', }}>
             <CardActionArea>
               <CardMedia
                 component="img"
                 height="100" // Adjust the height as needed
-                image={cajal} // Use the imgUrl variable instead of the img element
-                alt="cajal"
+                image={robdoc} // Use the imgUrl variable instead of the img element
+                alt="artistic interpretation of biological neural network"
               />
               <CardContent>
-                <Typography gutterBottom variant="h5" component="div" color = "#000">
+                <Typography gutterBottom variant="h5" component="div" >
                   <Link
                   overlay
                   underline = "none"
                   rel="noreferrer" // this is bad for anaylitics but good security
                   href = "https://github.com/andarklabs/cajal"
-                  color='#000'
                   target="_blank"
                   >
-                  Cajal
+                  Medical LLM
                   </Link>
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant="body2" color="neutral">
                   Neural Network Library for quick machine learning in C++.
                 </Typography>
               </CardContent>
@@ -84,56 +85,54 @@ function Projects(){
           </Card>
         </Stack>
         <Stack direction='row'  spacing={5} alignItems="center" >
-          <Card variant = "outlined" sx={{ maxWidth: 200, maxHeight:220}} style = {{ backgroundColor: '#90a4ae', boxShadow: '10px 10px 10px rgba(0,0,0, 0.99)', }} color = "#FFF">
+        <Card variant = "soft" color= 'primary' sx={{ maxWidth: 200, minWidth: 200, maxHeight:220, minHeight:220}} style = {{ backgroundColor: '#CCCCCC', boxShadow: '10px 10px 10px rgba(0,0,0, 0.99)', }}>
             <CardActionArea>
               <CardMedia
                 component="img"
                 height="100" // Adjust the height as needed
-                image={cajal} // Use the imgUrl variable instead of the img element
+                image={website} // Use the imgUrl variable instead of the img element
                 alt="cajal"
               />
               <CardContent>
-                <Typography gutterBottom variant="h5" component="div" color = "#000">
+                <Typography gutterBottom variant="h5" component="div" >
                   <Link
                   overlay
                   underline = "none"
                   rel="noreferrer" // this is bad for anaylitics but good security
                   href = "https://github.com/andarklabs/cajal"
-                  color='#000'
                   target="_blank"
                   >
-                  Cajal
+                  This Website
                   </Link>
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Neural Network Library for quick machine learning in C++.
+                <Typography variant="body2" color="neutral">
+                  Dynamic portfolio page.
                 </Typography>
               </CardContent>
             </CardActionArea>
           </Card>
-          <Card variant = "outlined" sx={{ maxWidth: 200, maxHeight:220}} style = {{ backgroundColor: '#90a4ae', boxShadow: '10px 10px 10px rgba(0,0,0, 0.99)', }} color = "#FFF">
+          <Card variant = "soft" color= 'primary' sx={{ maxWidth: 200, minWidth: 200, maxHeight:220, minHeight:220}} style = {{ backgroundColor: '#CCCCCC', boxShadow: '10px 10px 10px rgba(0,0,0, 0.99)', }}>
             <CardActionArea>
               <CardMedia
                 component="img"
                 height="100" // Adjust the height as needed
-                image={cajal} // Use the imgUrl variable instead of the img element
+                image={hyper} // Use the imgUrl variable instead of the img element
                 alt="cajal"
               />
               <CardContent>
-                <Typography gutterBottom variant="h5" component="div" color = "#000">
+                <Typography gutterBottom variant="h5" component="div" >
                   <Link
                   overlay
                   underline = "none"
                   rel="noreferrer" // this is bad for anaylitics but good security
-                  href = "https://github.com/andarklabs/cajal"
-                  color='#000'
+                  href = "https://github.com/aceniccola/hyperjumps"
                   target="_blank"
                   >
-                  Cajal
+                  Hyperjumps
                   </Link>
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Neural Network Library for quick machine learning in C++.
+                <Typography variant="body2" color="neutral">
+                  General solution to puzzle made by Quanta Magazine. 
                 </Typography>
               </CardContent>
             </CardActionArea>
