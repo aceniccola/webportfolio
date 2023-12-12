f6f9ddf17a5beb4f30916dd8355b26e5e47a973d
Andrew Ceniccola <andrewceniccola@Andrews-MacBook-Pro.local>
2023-12-11 22:46:45 -0500

    contact me

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,15 +4,19 @@ import { Paper, Typography, TextField } from '@mui/material';
 
 function Contact(){
     return (
-        <Card>
-            <Paper>
-                <Stack direction='row'>
-                    <Typography> Lets Talk! </Typography>
-                    <TextField id="firstname" label="First Name" variant="outlined"/>
-                    <TextField id="lastname" label="Last Name" variant="outlined"/>
-                </Stack>
-            </Paper>
-        </Card>
+        <div style={{marginTop:'210px', alignItems: 'center',  flexDirection: 'column', display: 'flex', }} justifyContent='center'>
+            <Card variant = "outlined" color="primary" style = {{ backgroundColor: '#E5E5E5', boxShadow: '10px 10px 10px rgba(0,0,0, 0.99)', }} sx = {{Height:250, minWidth:450, maxWidth:450}}>
+                    <Stack  spacing = {2} alignItems="center" >
+                        <Typography> Lets Talk! </Typography>
+                        <Stack direction='row' spacing={7.5} alignItems="center" justifyContent="space-between">
+                            <TextField id="firstname" label="First Name" variant="outlined" type = 'name' maxRows={1}/>
+                            <TextField id="lastname" label="Last Name" variant="outlined" type = 'name' maxRows={1}/>
+                        </Stack>
+                        <TextField id="email" label="Email Address" variant="outlined" type='email' fullWidth = {true} maxRows={1} />
+                        <TextField id="message" label = "Cat memes, puzzles and beta spray..." variant='outlined' fullWidth = {true} multiline minRows={5}/>
+                    </Stack>
+            </Card>
+        </div>
     );
 };
 export default Contact;
