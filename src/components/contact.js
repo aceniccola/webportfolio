b4d4093d452558a9828ae69fbee604edea281d45
Andrew Ceniccola <andrewceniccola@Andrews-MacBook-Pro.local>
2023-12-17 23:01:56 -0500

    server

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import { useState } from 'react'; // Import useState
 import { Card, Stack, Button } from '@mui/joy';
 import { Paper, Typography, TextField } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import axios from 'axios';
 
-function Contact(){
+function Contact() {
+    // State variable to hold the Typography content
+    const [typographyContent, setTypographyContent] = useState('Lets Talk!');
+
     const handleSubmit = () => {
         const data = {
             firstname: document.getElementById('firstname').value,
@@ -12,10 +16,16 @@ function Contact(){
             email: document.getElementById('email').value,
             message: document.getElementById('message').value,
         };
-        // TRITMaohieorRG3
+
         axios.post('http://localhost:3001/send', data)
             .then(response => {
-                console.log('Message sent', response);
+                // Conditional logic based on the message
+                if (data.message === 'q84hganIom94gbua$vN0q4gha5-iau&rbnga87598b') {
+                    setTypographyContent('Congratulations', data.firstname, 'for finding the 1st flag');
+                } else { 
+                    setTypographyContent(response.data.responseText); // Default case
+                }
+
                 // Clear the fields after successful post
                 document.getElementById('firstname').value = '';
                 document.getElementById('lastname').value = '';
@@ -26,23 +36,25 @@ function Contact(){
                 console.error('Error sending message', error);
             });
     };
+
     return (
-        <div style={{marginTop:'210px', alignItems: 'center',  flexDirection: 'column', display: 'flex', }} justifyContent='center'>
-            <Card variant = "outlined" color="primary" style = {{ backgroundColor: '#E5E5E5', boxShadow: '10px 10px 10px rgba(0,0,0, 0.99)', }} sx = {{Height:250, minWidth:450, maxWidth:450}}>
-                <Stack  spacing = {2} alignItems="center" >
-                    <Typography> Lets Talk! </Typography>
+        <div style={{ marginTop: '210px', alignItems: 'center', flexDirection: 'column', display: 'flex', }} justifyContent='center'>
+            <Card variant="outlined" color="primary" style={{ backgroundColor: '#E5E5E5', boxShadow: '10px 10px 10px rgba(0,0,0, 0.99)', }} sx={{ Height: 250, minWidth: 450, maxWidth: 450 }}>
+                <Stack spacing={2} alignItems="center">
+                    <Typography> {typographyContent} </Typography>
                     <Stack direction='row' spacing={7.5} alignItems="center" justifyContent="space-between">
-                        <TextField id="firstname" label="First Name" variant="outlined" type = 'name' maxRows={1}/>
-                        <TextField id="lastname" label="Last Name" variant="outlined" type = 'name' maxRows={1}/>
+                        <TextField id="firstname" label="First Name" variant="outlined" type='name' maxRows={1} />
+                        <TextField id="lastname" label="Last Name" variant="outlined" type='name' maxRows={1} />
                     </Stack>
-                    <TextField id="email" label="Email Address" variant="outlined" type='email' fullWidth = {true} maxRows={1}/>
-                    <TextField id="message" label = "Cat memes, puzzles and beta spray..." variant='outlined' fullWidth = {true} multiline minRows={5}/>
-                    <Button variant="outlined" startIcon={<SendIcon/>} onClick={handleSubmit}>
-                    Send
-                    </Button>   
+                    <TextField id="email" label="Email Address" variant="outlined" type='email' fullWidth={true} maxRows={1} />
+                    <TextField id="message" label="Cat memes, puzzles and beta spray..." variant='outlined' fullWidth={true} multiline minRows={5} />
+                    <Button variant="outlined" startIcon={<SendIcon />} onClick={handleSubmit}>
+                        Send
+                    </Button>
                 </Stack>
             </Card>
         </div>
     );
-};
+}
+
 export default Contact;
